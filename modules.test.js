07dee37432ac043d54323edfde6dfe8a0017df55
John Kibirige <kibirigejohn005@gmail.com>
2022-08-16 22:21:53 +0300

    Add reverse string method plus its unit tests

--- modules.test.js
+++ modules.test.js
@@ -1,5 +1,7 @@
 const stringLength = require('./modules/stringLength');
+const reverseString = require('./modules/reverseString');
 
+// unit tests for string length method
 test('Function created should return the correct length of the string', () => {
   expect(stringLength('dance')).toBe(5);
 });
@@ -8,4 +10,13 @@ test('Function should throw an error if the string length is less than 1', () =>
   expect(() => stringLength('')).toThrow(
     'Sorry string must have at least one character'
   );
+});
+
+// unit test for reverse string method
+test('Function should reverse a string ', () => {
+  expect(reverseString('abcd')).toBe('dcba');
+});
+
+test('function should return an empty string is passed to it', () => {
+  expect(reverseString('')).toBe('');
 });
